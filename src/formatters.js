0fa92890c0b483635d77096ad92bd8a65b40e185
estagent <estagent@gmail.com>
2021-08-09 03:32:16 +0300

    3.0.2 - formatDate added

--- src/formatters.js
+++ src/formatters.js
@@ -1,10 +1,22 @@
 import Preference from '@revgaming/preference'
 
 export default {
-  currencyAmount(currency_code, amount) {
-    amount.toLocaleString(Preference.locale(), {
-      style: 'currency',
-      currency: currency_code,
-    })
-  },
+    currencyAmount(currency_code, amount) {
+        return amount.toLocaleString(Preference.locale(), {
+            style: 'currency',
+            currency: currency_code,
+        })
+    },
+    formatDate(date) {
+        if (!date)
+            return ''
+        if (!(date instanceof Date))
+            date = new Date(date)
+        return date
+            .toLocaleString(Preference.locale(), {
+                day: '2-digit', // numeric, 2-digit
+                year: 'numeric', // numeric, 2-digit
+                month: '2-digit', // numeric, 2-digit, long, short, narrow
+            })
+    },
 }
