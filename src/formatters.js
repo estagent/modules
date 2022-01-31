5757868cfa1dfca3f4a6bc0be8d3e93227313b12
estagent <estagent@gmail.com>
2022-01-31 10:16:30 +0300

    5.3.13 - formatters

--- src/formatters.js
+++ src/formatters.js
@@ -1,22 +1,39 @@
 import Preference from '@revgaming/preference'
 
 export default {
-    currencyAmount(currency_code, amount) {
-        return amount.toLocaleString(Preference.locale(), {
-            style: 'currency',
-            currency: currency_code,
-        })
-    },
-    formatDate(date) {
-        if (!date)
-            return ''
-        if (!(date instanceof Date))
-            date = new Date(date)
-        return date
-            .toLocaleString(Preference.locale(), {
-                day: '2-digit', // numeric, 2-digit
-                year: 'numeric', // numeric, 2-digit
-                month: '2-digit', // numeric, 2-digit, long, short, narrow
-            })
-    },
+  currencyAmount(currency_code, amount, decimals = 2) {
+    return amount.toLocaleString(Preference.locale(), {
+      style: 'currency',
+      currency: currency_code.toUpperCase(),
+      minimumFractionDigits: 0,
+      maximumFractionDigits: decimals,
+    })
+  },
+  formatDate(date) {
+    if (!date)
+      return ''
+    if (!(date instanceof Date))
+      date = new Date(date)
+    return date
+      .toLocaleString(Preference.locale(), {
+        day: '2-digit', // numeric, 2-digit
+        year: 'numeric', // numeric, 2-digit
+        month: '2-digit', // numeric, 2-digit, long, short, narrow
+      })
+  },
+  formatTime(date) {
+    if (!date)
+      return ''
+    if (!(date instanceof Date))
+      date = new Date(date)
+    return date
+      .toLocaleString(Preference.locale(), {
+        day: '2-digit', // numeric, 2-digit
+        year: 'numeric', // numeric, 2-digit
+        month: '2-digit', // numeric, 2-digit, long, short, narrow
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+  },
 }
